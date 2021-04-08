c1cbc389967094061348ef9d562047d2f8c7bfba
Jonathan Fox <FoxJonathanP@gmail.com>
2021-04-07 20:43:36 -0700

    Finished all middleware functions

--- api/middleware/middleware.js
+++ api/middleware/middleware.js
@@ -1,5 +1,6 @@
+const User = require('../users/users-model');
+
 function logger(req, res, next) {
-  // DO YOUR MAGIC
   const method = req.method;
   const url = req.originalUrl;
   const date = new Date();
@@ -8,21 +9,36 @@ function logger(req, res, next) {
 }
 
 function validateUserId(req, res, next) {
-  // DO YOUR MAGIC
-  console.log('Running validateUserId middleware');
-  next();
+  User.getById(req.params.id)
+    .then(user => {
+      if (user) {
+        next();
+      }
+      else {
+        res.status(404).json({ message: 'user not found' });
+      }
+    })
+    .catch(err => {
+      res.status(500).json({ message: err.message });
+    });
 }
 
 function validateUser(req, res, next) {
-  // DO YOUR MAGIC
-  console.log('Running validateUser middleware');
-  next();
+  if (!req.body.name) {
+    res.status(400).json({ message: "missing required name field" });
+  }
+  else {
+    next();
+  }
 }
 
 function validatePost(req, res, next) {
-  // DO YOUR MAGIC
-  console.log('Running validatePost middleware');
-  next();
+  if (!req.body.text) {
+    res.status(400).json({ message: "missing required text field" });
+  }
+  else {
+    next();
+  }
 }
 
 // do not forget to expose these functions to other modules
