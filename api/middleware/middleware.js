e85961a50e33865b7e09c22305c12e9ab655d989
Jonathan Fox <FoxJonathanP@gmail.com>
2021-04-07 19:53:00 -0700

    impplemented logger

--- api/middleware/middleware.js
+++ api/middleware/middleware.js
@@ -1,17 +1,34 @@
 function logger(req, res, next) {
   // DO YOUR MAGIC
+  const method = req.method;
+  const url = req.originalUrl;
+  const date = new Date();
+  console.log(`[${date.toLocaleString()}] ${method} to ${url}`);
+  next();
 }
 
 function validateUserId(req, res, next) {
   // DO YOUR MAGIC
+  console.log('Running validateUserId middleware');
+  next();
 }
 
 function validateUser(req, res, next) {
   // DO YOUR MAGIC
+  console.log('Running validateUser middleware');
+  next();
 }
 
 function validatePost(req, res, next) {
   // DO YOUR MAGIC
+  console.log('Running validatePost middleware');
+  next();
 }
 
 // do not forget to expose these functions to other modules
+module.exports = {
+  logger,
+  validateUserId,
+  validateUser,
+  validatePost
+}
